046ac6e3f701814e571e932492d0eab8be4f4e07
Tomhero200 <tomhero200@merz-adendorf.de>
2025-02-22 00:30:41 +0100

    Fixed data-final-fixes.lua

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,7 +1,7 @@
 --data-final-fixes.lua
 
 --Ensure the mods are loaded
-if mods["bobelectronics"] and mods[spiderbots] then
+if mods["bobelectronics"] and mods["spiderbots"] then
   data.raw["recipe"]["spiderbot"]={
     { "basic-circuit-board", 4 },
     { "iron-plate",         12 },
