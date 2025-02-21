c2ea432be7e60af034b21ac80d3f7280c2d27869
Tomhero200 <tomhero200@merz-adendorf.de>
2025-02-22 00:28:35 +0100

    Initial change

---
+++ data-final-fixes.lua
@@ -0,0 +1,11 @@
+--data-final-fixes.lua
+
+--Ensure the mods are loaded
+if mods["bobelectronics"] and mods[spiderbots] then
+  data.raw["recipe"]["spiderbot"]={
+    { "basic-circuit-board", 4 },
+    { "iron-plate",         12 },
+    { "inserter",           8 },
+    { "raw-fish",           1 },
+}
+end
