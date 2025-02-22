1a726400986596576954771ffd3e4af8872725b1
Tomhero200 <tomhero200@merz-adendorf.de>
2025-02-22 01:02:50 +0100

    Added verification for existance of recipe

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,13 +2,17 @@
 
 --Ensure the mods are loaded
 if mods["bobelectronics"] and mods["spiderbots"] then
-  data.raw["recipe"]["spiderbot"]={
-    ingredients = {
-    { "basic-circuit-board", 4 },
-    { "iron-plate",         12 },
-    { "inserter",           8 },
-    { "raw-fish",           1 },
-  },
-  result = "spiderbot",
-}
+  if data.raw["recipe"]["spidertron"] then
+    data.raw["recipe"]["spiderbot"] = 
+    {
+      ingredients = 
+      {
+        { "basic-circuit-board", 4 },
+        { "iron-plate",         12 },
+        { "inserter",           8 },
+        { "raw-fish",           1 },
+    },
+    result = "spiderbot",
+    }
+  end
 end
